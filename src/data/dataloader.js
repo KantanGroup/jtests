89c0e4ecad32323a4b5be57bf68022736d60a113
Tran Anh Tuan <tuan.tran@fujixerox.co.jp>
2017-03-02 18:05:59 +0900

    Add trend

--- src/data/dataloader.js
+++ src/data/dataloader.js
@@ -14,6 +14,29 @@ function getAppInformation(id) {
   return getJSONFromRelativeURL(`/appInformationSolrs/${id}`);
 }
 
+function getAppTrend(id) {
+  const params = id.split('___');
+  const countryCode = params[0];
+  const category = params[1];
+  const collection = params[2];
+  const appId = params[3];
+  // http://localhost:9000/api/v1/appTrendSolrs/search/findByCountryCodeAndCategoryAndCollectionAndAppId?countryCode=br&category=all&collection=topgrossing&appId=com.supercell.clashroyale
+  return getJSONFromRelativeURL(`/appTrendSolrs/search/findByCountryCodeAndCategoryAndCollectionAndAppId?countryCode=${countryCode}&category=${category}&collection=${collection}&appId=${appId}`)
+    .then((data) => {
+      const index = {};
+      /* eslint no-underscore-dangle: ["error", { "allow": ["_embedded"] }]*/
+      if (data._embedded) {
+        index.apps = data._embedded.appIndexSolrs;
+        index.apps.sort((a, b) => a.index - b.index);
+      }
+
+      if (data.page) {
+        index.page = data.page;
+      }
+      return index;
+    });
+}
+
 function getIndexApps(id) {
   const params = id.split('___');
   const countryCode = params[0];
@@ -50,8 +73,15 @@ const appIndexLoader =
     cacheMap,
   });
 
+const appTrendLoader =
+  new DataLoader(keys => Promise.all(keys.map(getAppTrend)), {
+    cacheKeyFn: key => `/apps/trend/${key}/`,
+    cacheMap,
+  });
+
 appLoader.loadApp = appLoader.load.bind(appLoader);
 appLoader.loadIndex = appIndexLoader.load.bind(appIndexLoader);
+appLoader.loadTrend = appTrendLoader.load.bind(appTrendLoader);
 
 const dataloader = {
   app: appLoader,
