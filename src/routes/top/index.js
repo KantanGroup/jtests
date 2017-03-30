1923927db1c89d99249c4e0f1fe10405d33a32c7
Tran Anh Tuan <tuan.tran@fujixerox.co.jp>
2017-03-30 18:08:45 +0900

    Redirect 301

--- src/routes/top/index.js
+++ src/routes/top/index.js
@@ -7,61 +7,15 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React from 'react';
-import fetch from '../../core/fetch';
-import Layout from '../../components/Layout';
-import Top from './Top';
-import { capitalize } from '../../common';
-
-/* eslint max-len: ["error", 1000]*/
 export default {
 
   path: '/top-mobile-app-trend-in-:countryName/:countryCode',
 
   async action({ params }) {
     const countryName = params.countryName;
-    const countryCode = params.countryCode;
-    const resp = await fetch('/graphql', {
-      method: 'post',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `{index(countryCode:"${countryCode}",category:"all"){topgrossing{index,appId,category,collection,countryCode,title,point,price,developerId}topsellingFree{index,appId,category,collection,countryCode,title,point,price,developerId}topsellingPaid{index,appId,category,collection,countryCode,title,point,price,developerId}topsellingNewFree{index,appId,category,collection,countryCode,title,point,price,developerId}topsellingNewPaid{index,appId,category,collection,countryCode,title,point,price,developerId}}}`,
-      }),
-      credentials: 'include',
-    });
-    const { data } = await resp.json();
-    let component;
-    if (data.index) {
-      component = (
-        <Layout countryCode={countryCode}>
-          <Top
-            topgrossing={data.index.topgrossing}
-            topsellingFree={data.index.topsellingFree}
-            topsellingPaid={data.index.topsellingPaid}
-            topsellingNewPaid={data.index.topsellingNewPaid}
-            topsellingNewFree={data.index.topsellingNewFree}
-            countryName={countryName}
-            countryCode={countryCode}
-          />
-        </Layout>
-      );
-    } else {
-      component = (
-        <Layout countryCode={countryCode}>
-          <Top
-            countryName={countryName}
-            countryCode={countryCode}
-          />
-        </Layout>
-      );
-    }
     return {
-      title: `Top mobile app trends in ${capitalize(countryName.split('-').join(' '))}`,
-      description: `Infographic highlighting the top mobile app trends in ${countryName.split('-').join(' ')}.`,
-      component,
+      redirect: `/top-mobile-app-trend-in-${countryName}/googlestore/top-app`,
+      status: 301,
     };
   },
 
